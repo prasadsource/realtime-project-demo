e7fe1578c59f3ae2d7b4ec96694eb8c2373b1ba4
Prasad Dhanikonda <prasadsource@gmail.com>
2023-02-02 16:30:00 +0530

    sysout to log

--- src/main/java/com/cloudtechmasters/realtimeprojectdemo/CustomerController.java
+++ src/main/java/com/cloudtechmasters/realtimeprojectdemo/CustomerController.java
@@ -1,5 +1,7 @@
 package com.cloudtechmasters.realtimeprojectdemo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,10 +9,12 @@ import java.util.Arrays;
 import java.util.List;
 @RestController
 public class CustomerController {
+	private static final Logger logger = LoggerFactory.getLogger(CustomerController.class);
     @GetMapping("/getAllCustomers")
     public List<Customer> getAllCustomers(){
         Customer customer1=new Customer(100,"vamsikrm");
         Customer customer2=new Customer(200,"harsha");
+        logger.info("Test Message:{}","This is my get Call");
         return Arrays.asList(customer1,customer2);
     }
 }
